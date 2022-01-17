ec9c9bba5f9c847ba2018129587b1e364d1ce758
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-01-17 12:52:19 +0100

    shows the random selected products for both tv and shop list

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@ class PhoneGame {
     //array of products
     this.products = ['fridge', 'blender', 'vacuum', 'toaster', 'washing machine', 'steam iron', 'lamp', 'air condition'];
     this.randomPhoneNum = Math.floor(100000000 + Math.random() * 900000000);
-    this.tvProduct = this.products[Math.floor(Math.random() * this.products.length)];
-    console.log(this.tvProduct);
     this.shopListProduct = this.products[Math.floor(Math.random() * this.products.length)];
     console.log(this.shopListProduct);
+    this.tvProduct = this.products[Math.floor(Math.random() * this.products.length)];
+    console.log(this.tvProduct);
   }
   //10s timer function
-  timer() {
+  /*timer() {
     var timeleft = 10;
 var gameTimer = setInterval(function () {
   if (timeleft <= 0) {
@@ -22,44 +22,14 @@ var gameTimer = setInterval(function () {
   }
   timeleft -= 1;
 }, 1000);
-  }
+  }*/
   //method to check if game is finished (right product && right phone number)
   /*checkIfFinished() {
     // ...
   }*/
 }
 
-//start the myGame object (from the class PhoneGame)
-const myGame = new PhoneGame();
-console.log(myGame.timer());
-//get Start Game button from html
-const startGameBtn = document.getElementById("btn-start-game");
-onclick.startGameBtn = startGame();
-
-//store random phone number on the random phone number element from html
-const randomPhone = document.getElementById("random-phone");
-let phoneNum = randomPhone.innerHTML;
-myGame.randomPhoneNum = phoneNum;
-
-//starts the game
-//when pressing the startGame button show random products on the tv and shopping list
-function startGame(){
-  console.log("i'm inside the startGame");
-
-
-  myGame.timer();
-  myGame.shuffleProductsTv();
-  myGame.shuffleProductsList();
-}
-
-
-//create class 'product'
-
-//create objects inside the class for each product(?)
-
-//get two random objects, one for tv one for shopping list
-
-//create the rotary dial
+//initialize rotary dial
 const init = function () {
   const div = document.createElement("div");
 
@@ -67,7 +37,7 @@ const init = function () {
 
   const btn = document.createElement("button");
 
-  btn.innerText = "Clear";
+  btn.innerText = "Call";
 
   btn.addEventListener("click", (e) => {
     input.value = "";
@@ -89,3 +59,42 @@ const init = function () {
 };
 
 init();
+
+//start the myGame object (from the class PhoneGame)
+const myGame = new PhoneGame();
+
+//show shopping list
+let shopProduct = document.getElementById("shopProduct");
+shopProduct.textContent = myGame.shopListProduct;
+//show tv list
+let productTv = document.getElementById("tv-product");
+productTv.textContent = myGame.tvProduct;
+
+
+//get Start Game button from html
+const startGameBtn = document.getElementById("btn-start-game");
+startGameBtn.addEventListener("click", startGame);
+//startGameBtn.onclick = startGame();
+//console.log(startGameBtn.onclick, 'clicked');
+
+//store random phone number on the random phone number element from html
+const randomPhone = document.getElementById("random-phone");
+let phoneNum = randomPhone.innerHTML;
+myGame.randomPhoneNum = phoneNum;
+
+//starts the game
+//when pressing the startGame button show random products on the tv and shopping list
+function startGame(){
+  console.log("i'm inside the startGame");
+  
+  //myGame.timer();
+}
+
+
+//create class 'product'
+
+//create objects inside the class for each product(?)
+
+//get two random objects, one for tv one for shopping list
+
+
